6facf330f12bce402014527e38f9c594c40d9703
juanMSLV <jms-martinez-22@live.com.mx>
2019-05-11 23:13:07 -0500

    Add files via upload

---
+++ web/resource/scrips/diario.js
@@ -0,0 +1,263 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function TypeDiario() {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("listaMeses");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        
+    }
+   
+    var cod = document.getElementById("listaUsuarios").value;
+    if(document.getElementById("listaUsuarios").value==="Enero"){
+    
+    document.getElementById("Enero").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    
+   
+    var y = document.getElementsByClassName("Enero");
+    var nutri = document.getElementsByClassName("Enero");
+    nutri[0].style += "background: white;";
+    
+}
+
+if(document.getElementById("listaUsuarios").value==="Febrero"){
+    
+    document.getElementById("Febrero").style.display = "block";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Febrero");
+    var pacien = document.getElementsByClassName("Febrero");
+    pacien[0].style += "background: white;";
+}
+
+    if(document.getElementById("listaUsuarios").value==="Marzo"){
+   
+    document.getElementById("Marzo").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Marzo");
+    var psico = document.getElementsByClassName("Marzo");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Abril"){
+   
+    document.getElementById("Abril").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Abril");
+    var psico = document.getElementsByClassName("Abril");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Mayo"){
+   
+    document.getElementById("Mayo").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Mayo");
+    var psico = document.getElementsByClassName("Mayo");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Junio"){
+   
+    document.getElementById("Junio").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Junio");
+    var psico = document.getElementsByClassName("Junio");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Julio"){
+   
+    document.getElementById("Julio").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Julio");
+    var psico = document.getElementsByClassName("Julio");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Agosto"){
+   
+    document.getElementById("Agosto").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Agosto");
+    var psico = document.getElementsByClassName("Agosto");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Septiembre"){
+   
+    document.getElementById("Septiembre").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Septiembre");
+    var psico = document.getElementsByClassName("Septiembre");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Octubre"){
+   
+    document.getElementById("Octubre").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Octubre");
+    var psico = document.getElementsByClassName("Octubre");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Noviembre"){
+   
+    document.getElementById("Noviembre").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    document.getElementById("Diciembre").style.display = "none";
+    var y = document.getElementsByClassName("Noviembre");
+    var psico = document.getElementsByClassName("Noviembre");
+    psico[0].style += "background: white;";
+}
+  if(document.getElementById("listaUsuarios").value==="Diciembre"){
+   
+    document.getElementById("Diciembre").style.display = "block";
+    document.getElementById("Febrero").style.display = "none";
+    document.getElementById("Marzo").style.display = "none";
+    document.getElementById("Abril").style.display = "none";
+    document.getElementById("Mayo").style.display = "none";
+    document.getElementById("Junio").style.display = "none";
+    document.getElementById("Julio").style.display = "none";
+    document.getElementById("Agosto").style.display = "none";
+    document.getElementById("Septiembre").style.display = "none";
+    document.getElementById("Octubre").style.display = "none";
+    document.getElementById("Noviembre").style.display = "none";
+    document.getElementById("Enero").style.display = "none";
+    var y = document.getElementsByClassName("Diciembre");
+    var psico = document.getElementsByClassName("Diciembre");
+    psico[0].style += "background: white;";
+}
+    
+    y[0].style = "background: rgb(108, 29, 69); color: white;";
+    
+
+}
+function TypeOptionDiario(fecha,sentimiento,contenido,observaciones,ID) {
+ 
+  
+  
+ 
+  document.getElementById("fecha").innerHTML=fecha;
+  document.getElementById("contenidoh").innerHTML=contenido;
+  document.getElementById("sentimiento").innerHTML=sentimiento;
+  document.getElementById("observaciones").innerHTML=observaciones;
+  document.getElementById("id_hoja").value=ID;
+  
+  
+  
+  
+  
+  
+
+ 
+   
+   
+
+}
